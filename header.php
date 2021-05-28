2c2282fcd360a5f1d5db5acea6af48d4ca7d2f93
YohannChavet <79133367+YohannChavet@users.noreply.github.com>
2021-05-28 19:57:08 +0200

    v2

--- header.php
+++ header.php
@@ -1,16 +1,48 @@
 <?php
 $user_connected = True;
 
-echo '<header><div class="accueil"><a href="./index.php">Accueil</a></div>
-<div class="rechercher"><a href="./rechercher.php">Rechercher</a></div>
-<div class="mes_quizz"><a href="./mes_quizz.php">Mes Quizz</a></div>';
+echo '<style>
+header {
+    background-color: #2e6da4;
+    color: #2e6da4;
+}
+.accueil-div, .rechercher-div {
+    float: left;
+    margin-left: 5%;
+    color: white;
+}
+</style>
+<header><div class="accueil-div"><a class="accueil-a" href="./index.php">Accueil</a></div>
+<div class="rechercher-div"><a class="rechercher-a" href="./rechercher.php">Rechercher</a></div>
+';
 
 if ($user_connected) {
-    echo '<div class="profil"><a href="./profil.php">Mon Profil</a></div>
-<div class="deconnexion"><a href="./deconnexion.php">Déconnexion</a></div></header>';
+    echo '<style>
+.profil-div, .deconnexion-div {
+    float: right;
+    margin-right: 5%;
+    color: white;
+}
+.mes_quizz-div {
+    float: left;
+    margin-left: 5%;
+    color: white;
+}
+</style>
+<div class="mes_quizz-div"><a class="mes_quizz-a" href="./mes_quizz.php">Mes Quizz</a></div>
+<div class="deconnexion-div"><a class="deconnexion-a" href="./deconnexion.php">Déconnexion</a></div>
+<div class="profil-div"><a class="profil-a" href="./profil.php">Mon Profil</a></div>
+</header>';
 } else {
-    echo '<div class="connexion"><a href="./connexion.php">Connexion</a></div>
-<div class="connexion"><a href="./inscription.php">Inscription</a></div></header>';
+    echo '<style>
+.connexion-div, .inscription-div {
+    float: right;
+    margin-right: 5%;
+    color: white;
+}
+</style>
+<div class="connexion-div"><a class="connexion-a" href="./connexion.php">Connexion</a></div>
+<div class="inscription-div"><a class="inscription-a" href="./inscription.php">Inscription</a></div></header>';
 }
 ?>
 
