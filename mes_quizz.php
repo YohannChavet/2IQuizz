4914665f95344570f2575986eb09a58b9273a9b0
YohannChavet <79133367+YohannChavet@users.noreply.github.com>
2021-05-28 21:42:03 +0200

    Add files via upload

--- mes_quizz.php
+++ mes_quizz.php
@@ -5,6 +5,7 @@ echo '<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="2i.png" />
   <title>Mes Quizz - 2IQuizz</title>
   <meta name="description" content="Visionnez et créez des quizz depuis cette page sur 2IQuizz">
 </head>
