42f0339b7a25086be306ecdd4252d7f5ef8baf28
YohannChavet <79133367+YohannChavet@users.noreply.github.com>
2021-05-31 16:32:18 +0200

    accueil UX

--- templates/accueil.php
+++ templates/accueil.php
@@ -1,1 +1,31 @@
-accueil
+<div class="flex">
+    <div class="left">
+        <div class="title">
+            <h1 class="title">2IQuizz</h1>
+        </div>
+        <div class="principle-header-div">
+            <div class="down-triangle"></div>
+            <div class="dash"></div>
+            <p class="principle-header-p">- Le principe du site -</p>
+            <div class="down-triangle"></div>
+            <div class="dash"></div>
+        </div>
+        <div class="principle-div">
+            <p class="principle-p">Des QCM sur différents thèmes au choix avec possibilité d'en créer <br/>
+                Testez votre culture scolaire de LE1 ou dans plein d'autres catégories ! <br/>
+                Il existe différents types de quizz divers et variés <br/>
+                Le site tient son nom car il vous est destiné</p> <br/>
+        </div>
+    </div>
+    <div class="right">
+        <div class="spikes">
+            <?php
+
+            for ($i = 1; $i <= 50; $i++) {
+                echo '<div class="spike"></div>';
+            }
+            ?>
+        </div>
+        <div class="right-box"><p class="accueil-p">Accueil</p></div>
+    </div>
+</div>
