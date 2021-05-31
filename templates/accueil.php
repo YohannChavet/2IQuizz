4bedf9434465bff7d879c04d2ec61066d8ecc5bb
YohannChavet <79133367+YohannChavet@users.noreply.github.com>
2021-06-01 01:12:22 +0200

    global UI Template

--- templates/accueil.php
+++ templates/accueil.php
@@ -18,19 +18,11 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
         <div class="title">
             <h1 class="title">2IQuizz</h1>
         </div>
-        <div class="principle-header-div">
-            <div class="down-triangle"></div>
-            <div class="dash"></div>
-            <p class="principle-header-p">- Le principe du site -</p>
-            <div class="down-triangle"></div>
-            <div class="dash"></div>
-        </div>
-        <div class="principle-div">
-            <p class="principle-p">Des QCM sur différents thèmes au choix avec possibilité d'en créer <br/>
+        <p class="principle-header-p">▼  ━  Le principe du site  ━  ▼</p>
+        <p class="principle-p">Des QCM sur différents thèmes au choix avec possibilité d'en créer <br/>
                 Testez votre culture scolaire de LE1 ou dans plein d'autres catégories ! <br/>
                 Il existe différents types de quizz divers et variés <br/>
                 Le site tient son nom car il vous est destiné</p> <br/>
-        </div>
     </div>
     <div class="right">
         <div class="spikes">
@@ -41,7 +33,7 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
             }
             ?>
         </div>
-        <div class="right-box"><p class="accueil-p">Accueil</p></div>
+        <div class="right-box"><p class="rtitle-p">Accueil</p></div>
     </div>
 </div>
 </body>
