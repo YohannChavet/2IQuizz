d301d16b798c64e3cc5eb900f092717f578aaa63
YohannChavet <79133367+YohannChavet@users.noreply.github.com>
2021-06-01 01:31:28 +0200

    profil

--- templates/profil.php
+++ templates/profil.php
@@ -1,1 +1,30 @@
-profil
+<?php
+
+if (basename($_SERVER["PHP_SELF"]) != "index.php")
+{
+    header("Location:../index.php?view=login");
+    die("");
+}
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<div class="flex">
+    <div class="left">
+    </div>
+    <div class="right">
+        <div class="spikes">
+            <?php
+
+            for ($i = 1; $i <= 50; $i++) {
+                echo '<div class="spike"></div>';
+            }
+            ?>
+        </div>
+        <div class="right-box"><p class="rtitle-p">Mon Profil</p></div>
+    </div>
+</div>
+</body>
