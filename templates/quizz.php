e15c24aa87cbbe5daa77f7c18504b9920784b3ce
YohannChavet <79133367+YohannChavet@users.noreply.github.com>
2021-06-03 16:45:39 +0200

    Add files via upload

--- templates/quizz.php
+++ templates/quizz.php
@@ -2,46 +2,29 @@
 // Ce fichier permet de tester les fonctions développées dans le fichier malibforms.php
 
 // Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-if (basename($_SERVER["PHP_SELF"]) == "quizz.php")
-{
-	header("Location:../index.php?view=quizz");
-	die("");
+if (basename($_SERVER["PHP_SELF"]) == "quizz.php") {
+    header("Location:../index.php?view=quizz");
+    die("");
 }
 
 
 echo $_GET['message'];
-$idQuizz=$_GET['IDQuizz'];
+$idQuizz = $_GET['IDQuizz'];
 $auteur = AuteurQuizz($_GET['IDQuizz']);
-if($auteur===$_SESSION["idUser"]){
-    if(IsVRaiFaux($_GET['IDQuizz'])===1){
-        mkForm('controleur.php','GET');
-        ?><p class="label">Indiquez le numéro de la question : </p><?php
-        mkInput('number','NQues',0);
-        ?><p class="label">Indiquez la question : </p><?php
-        mkInput('text','Question','');
+if ($auteur === $_SESSION["idUser"]){
+if (IsVRaiFaux($_GET['IDQuizz']) === 1){
+mkForm('controleur.php', 'GET');
 ?>
-
-<p class="label">Vrai ou Faux : </p>
-<select name="VraiFaux">
-    <option value="1" >Vrai</option>
-    <option value="0">Faux</option>
-  </select>
-
-<?php
-        mkInput('submit','action','Créer une question');
-        mkInput('submit','action','Modifier une question');
-        mkInput('submit','action','Supprimer la question');
-        mkInput('hidden','type','VraiFaux');
-        mkInput('hidden','IDQuizz',$idQuizz);
-        endForm();
-        $quizz=listerquestionVF($_GET['IDQuizz']);  
+<div class="flex">
+    <div class="left">
+        <?php $quizz = listerquestionVF($_GET['IDQuizz']);
         echo "<table>";
         echo "<tr>
               <th scope=\"col\">Numéro</th>
               <th scope=\"col\">Question</th>
               <th scope=\"col\">Vrai/Faux</th>
               </tr>";
-        foreach($quizz as $ligne){
+        foreach ($quizz as $ligne) {
             echo "<tr>";
             echo "<th class=\"scope\"   >";
             echo $ligne['N_Question'];
@@ -53,29 +36,67 @@ if($auteur===$_SESSION["idUser"]){
             echo $ligne['VraiFaux'];
             echo "</th>";
             echo "</tr>";
-        }
-    }
-else{
-    mkForm('controleur.php','GET');
+        } ?>
+        </table>
+    </div>
+    <div class="right">
+        <div class="spikes">
+            <?php
+
+            for ($i = 1; $i <= 50; $i++) {
+                echo '<div class="spike"></div>';
+            }
+            ?>
+        </div>
+        <div class="right-box"><p class="rtitle-p">Créer une Question</p>
+            <p class="label">Indiquez le numéro de la question : </p><?php
+            mkInput('number', 'NQues', 0);
+            ?><p class="label">Indiquez la question : </p>
+            <div class="grow-wrap">
+                <textarea name="Question" onInput="this.parentNode.dataset.replicatedValue = this.value"></textarea>
+            </div>
+            <?php
+            //mkInput('textarea', 'Question', '');
+            ?>
+
+            <p class="label">Vrai ou Faux : </p>
+            <select name="VraiFaux">
+                <option value="1">Vrai</option>
+                <option value="0">Faux</option>
+            </select>
+
+            <?php
+            mkInput('submit', 'action', 'Créer une question');
+            mkInput('submit', 'action', 'Modifier une question');
+            mkInput('submit', 'action', 'Supprimer la question');
+            mkInput('hidden', 'type', 'VraiFaux');
+            mkInput('hidden', 'IDQuizz', $idQuizz);
+            endForm(); ?>
+        </div>
+    </div>
+</div>
+<?php
+} else {
+    mkForm('controleur.php', 'GET');
     ?><p class="mkquizz">Indiquez le numéro de la question : </p><?php
-    mkInput('number','NQues','');
+    mkInput('number', 'NQues', '');
     ?><p class="mkquizz">Indiquez la question : </p><?php
-    mkInput('text','Question','');
+    mkInput('text', 'Question', '');
     ?><p class="mkquizz">Indiquez la réponse: </p><?php
-    mkInput('text','Reponse','');
+    mkInput('text', 'Reponse', '');
     ?><p class="mkquizz">Indiquez le choix 1 : </p><?php
-    mkInput('text','CHOIX1','');
+    mkInput('text', 'CHOIX1', '');
     ?><p class="mkquizz">Indiquez le choix 2 : </p><?php
-    mkInput('text','CHOIX2','');
+    mkInput('text', 'CHOIX2', '');
     ?><p class="mkquizz">Indiquez le choix 3 : </p><?php
-    mkInput('text','CHOIX3','');
-    mkInput('submit','action','Créer une question');
-    mkInput('submit','action','Modifier une question');
-    mkInput('submit','action','Supprimer la question');
-    mkInput('hidden','type','QCM');
-    mkInput('hidden','IDQuizz',$idQuizz);
+    mkInput('text', 'CHOIX3', '');
+    mkInput('submit', 'action', 'Créer une question');
+    mkInput('submit', 'action', 'Modifier une question');
+    mkInput('submit', 'action', 'Supprimer la question');
+    mkInput('hidden', 'type', 'QCM');
+    mkInput('hidden', 'IDQuizz', $idQuizz);
     endForm();
-    $quizz=listerquestionQCM($_GET['IDQuizz']);  
+    $quizz = listerquestionQCM($_GET['IDQuizz']);
     echo "<table>";
     echo "<tr>
           <th scope=\"col\">Numéro</th>
@@ -85,7 +106,7 @@ else{
           <th scope=\"col\">Choix2</th>
           <th scope=\"col\">Choix3</th>
           </tr>";
-    foreach($quizz as $ligne){
+    foreach ($quizz as $ligne) {
         echo "<tr>";
         echo "<th class=\"scope\"   >";
         echo $ligne['N_Question'];
@@ -108,47 +129,32 @@ else{
         echo "</tr>";
     }
 }
-}
-else{
+} else {
 
 
-    if(IsVRaiFaux($_GET['IDQuizz'])==1){
-    $quizz=listerquestionVF($_GET['IDQuizz']); 
-    if (empty($quizz)==0){
+    if (IsVRaiFaux($_GET['IDQuizz']) == 1) {
+        $quizz = listerquestionVF($_GET['IDQuizz']);
+        var_dump($quizz);
+    } else {
+
+        $quizz = listerquestionQCM($_GET['IDQuizz']);
+        foreach ($quizz as $ligne) {
+            $table = tabale();
+            $asso = array(
+                'Reponse' => $ligne['Reponse'],
+                'PosR' => $table[0],
+                'CHOIX1' => $ligne['CHOIX1'],
+                'PosC1' => $table[1],
+                'CHOIX2' => $ligne['CHOIX2'],
+                'PosC2' => $table[2],
+                'CHOIX3' => $ligne['CHOIX3'],
+                'PosC3' => $table[3]
+            );
+        }
         mkForm('controleur.php', 'GET');
-        afficherquestionVF($quizz);
-        mkInput("submit","action","valider");
-        }
-        else{
-            echo"Quizz vide, sélectionnez un autre quizz";
-    
-        }
+        afficherquestionQCM($quizz, $asso);
+        mkInput("submit", "action", "valider");
     }
-    else{
-    $quizz=listerquestionQCM($_GET['IDQuizz']); 
-    if (empty($quizz)==0){
-    foreach($quizz as $ligne){
-        $table=tabale();
-        $asso=array(
-            'Reponse' => $ligne['Reponse'],
-            'PosR' => $table[0],
-            'CHOIX1' => $ligne['CHOIX1'],
-            'PosC1' => $table[1],
-            'CHOIX2' => $ligne['CHOIX2'],
-            'PosC2' => $table[2],
-            'CHOIX3' => $ligne['CHOIX3'],
-            'PosC3' => $table[3]
-        );
-    }
-    mkForm('controleur.php', 'GET');
-    afficherquestionQCM($quizz,$asso);
-    mkInput("submit","action","valider");
-    }
-    else{
-        echo"Quizz vide, sélectionnez un autre quizz";
-
-    }
-}
 }
 
 ?>
